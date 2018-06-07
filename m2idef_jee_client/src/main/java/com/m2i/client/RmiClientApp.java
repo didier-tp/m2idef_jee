456726a4e616d7c2c0a516db04abee631649567e
didier <didier@ici_ou_labas.fr>
2018-06-07 12:16:30 +0200

    nouvelle version

---
+++ m2idef_jee_client/src/main/java/com/m2i/client/RmiClientApp.java
@@ -0,0 +1,42 @@
+package com.m2i.client;
+
+import java.util.Properties;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+
+import com.m2i.service.IServiceDevise;
+
+public class RmiClientApp {
+
+	public static void main(String[] args) throws Exception {
+        Properties props = new Properties(); //de java.util
+        //Context et InitialContext de javax.naming (JNDI)
+		props.put(Context.INITIAL_CONTEXT_FACTORY, 
+				   "org.jboss.naming.remote.client.InitialContextFactory");
+		props.put(Context.PROVIDER_URL, "http-remoting://localhost:8080");
+		// remote://localhost:4447 for Jboss7.1 , http-remoting://localhost:8080
+		// for wildfly 8,9
+		props.put(Context.SECURITY_PRINCIPAL, "guest"); // username : "admin" ,
+														// "guest" , "..."
+		props.put(Context.SECURITY_CREDENTIALS, "guest007"); // password :
+																// "pwd",
+																// "guest007"
+		// avec utilisateur ajouté via la commande JBOSS_7_HOME/bin/add-user
+		// et roles associés admin,guest,.... sur partie "ApplicationRealm" .
+		props.put("jboss.naming.client.ejb.context", true); // indispensable
+															// pour accès
+															// @Remote
+		Context jndiContext = new InitialContext(props);
+		String jndiName = "m2idef_jee_ear/m2idef_jee_ejb_impl/ServiceDeviseBean" 
+		                  + "!com.m2i.service.IServiceDevise";
+		// sans "ejb:" et sans // pour version de jndiName sans
+		// jboss-ejb-client.properties et sans
+		// props.setProperty(Context.URL_PKG_PREFIXES,"org.jboss.ejb.client.naming");
+		
+		IServiceDevise ejbServiceDevise = (IServiceDevise) 
+				                        jndiContext.lookup(jndiName);
+		System.out.println("30 euros= " +ejbServiceDevise.euroToFranc(30)+ " francs");
+
+	}
+}
